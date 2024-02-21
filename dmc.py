ca80edf6fbc0abe9a9a7beeaef1f5027c4ba86f9
Jacoroch <110855576+Jacoroch@users.noreply.github.com>
2024-02-21 17:55:38 -0500

    Update dmc.py

--- dmc.py
+++ dmc.py
@@ -15,7 +15,7 @@ def convert_to_mp3(file_path, output_path, start_time):
     audio.export(output_path, format="mp3")
     local_end_time = time.time()
     elapsed_end = local_end_time - start_time
-    print(f"Conversión a MP3 completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.2f}s y terminó en t={elapsed_end:.2f}s")
+    print(f"Conversión a MP3 completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.3f}s y terminó en t={elapsed_end:.3f}s")
 
 def convert_to_wav(file_path, output_path, start_time):
     local_start_time = time.time()
@@ -24,7 +24,7 @@ def convert_to_wav(file_path, output_path, start_time):
     audio.export(output_path, format="wav")
     local_end_time = time.time()
     elapsed_end = local_end_time - start_time
-    print(f"Conversión a WAV completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.2f}s y terminó en t={elapsed_end:.2f}s")
+    print(f"Conversión a WAV completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.3f}s y terminó en t={elapsed_end:.3f}s")
 
 def convert_to_m4a(file_path, output_path, start_time):
     local_start_time = time.time()
@@ -33,7 +33,7 @@ def convert_to_m4a(file_path, output_path, start_time):
     audio.export(output_path, format="ipod", codec="aac")
     local_end_time = time.time()
     elapsed_end = local_end_time - start_time
-    print(f"Conversión a M4A completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.2f}s y terminó en t={elapsed_end:.2f}s")
+    print(f"Conversión a M4A completada. Tamaño: {get_file_size(output_path)} bytes. Comenzó en t={elapsed_start:.3f}s y terminó en t={elapsed_end:.3f}s")
 
 def process_directory(directory_path, output_format, start_time, max_workers=13):
     print("workers: "+ str(max_workers))
@@ -54,7 +54,6 @@ def process_directory(directory_path, output_format, start_time, max_workers=13)
                     # Enviar la ruta de salida correcta junto con el formato y el tiempo de inicio
                     executor.submit(convert_file_to_format, file_path, output_path, output_format, start_time)
 
-
 def convert_file_to_format(file_path, output_path, output_format, start_time):
     if output_format == "mp3":
         convert_to_mp3(file_path, output_path, start_time)
@@ -62,7 +61,6 @@ def convert_file_to_format(file_path, output_path, output_format, start_time):
         convert_to_wav(file_path, output_path, start_time)
     elif output_format == "m4a":
         convert_to_m4a(file_path, output_path, start_time)
-
 
 def process_single_file(file_path, start_time):
     #nombre de los archivos de salida
@@ -101,22 +99,32 @@ def process_single_file(file_path, start_time):
 
 if __name__ == "__main__":
     start_time = time.time()
-    
-    parser = argparse.ArgumentParser(description='Disc Music Compressor: Convierte archivos a MP3, WAV y M4A')
-    parser.add_argument('-f', '--file', help='El nombre del archivo o directorio a convertir', required=True)
-    parser.add_argument('-e', '--extension', help='Formato de salida para la conversión (requerido solo para directorios)')
-    
-    args = parser.parse_args()
+    exit_status = 0  # Estado de salida predeterminado como "éxito"
 
-    if os.path.isdir(args.file):
-        if not args.extension or args.extension not in ['mp3', 'wav', 'm4a']:
-            print("Error: Debes especificar un formato de salida válido (-e mp3, wav, m4a) para procesar un directorio.")
+    try:
+        parser = argparse.ArgumentParser(description='Disc Music Compressor')
+        parser.add_argument('-f', '--file', help='El nombre del archivo o directorio a convertir', required=True)
+        parser.add_argument('-e', '--extension', help='Formato de salida para la conversión (requerido solo para directorios)', default='')
+
+        args = parser.parse_args()
+
+        if os.path.isdir(args.file):
+            if not args.extension or args.extension not in ['mp3', 'wav', 'm4a']:
+                print("Error: Debes especificar un formato de salida válido (-e mp3, wav, m4a) para procesar una carpeta.")
+                exit_status = 1  
+            else:
+                process_directory(args.file, args.extension, start_time)
+        elif os.path.isfile(args.file):
+            if args.file.lower().endswith('.aif'):
+                process_single_file(args.file, start_time)
+            else:
+                print("Error: El archivo especificado no es un archivo .aif")
+                exit_status = 1  
         else:
-            process_directory(args.file, args.extension, start_time)
-    elif os.path.isfile(args.file):
-        if args.file.lower().endswith('.aif'):
-            process_single_file(args.file, start_time)  
-        else:
-            print("Error: El archivo especificado no es un archivo .aif")
-    else:
-        print("El archivo o directorio especificado no existe.")
+            print("El archivo o directorio especificado no existe.")
+            exit_status = 1  
+    except Exception as e:
+        print(f"Ocurrió un error durante la ejecución: {e}")
+        exit_status = 1  
+    finally:
+        print("Estado de salida del proceso:"+ str(exit_status))
